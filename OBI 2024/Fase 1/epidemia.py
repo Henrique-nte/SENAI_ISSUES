6cc7fb062c4a2cd317b410300490ed6f154c67e5
Henrique Santos <silvashrq@gmail.com>
2025-03-01 12:57:36 -0300

    Update epidemia.py

--- OBI 2024/Fase 1/epidemia.py
+++ OBI 2024/Fase 1/epidemia.py
@@ -4,22 +4,25 @@
 #(3 + 6 + 12 + 24 = 45 pessoas no total), e assim por diante.
 #Dados o número inicial de pessoas infectadas no dia 0 e o fator reprodutivo R da epidemia, escreva
 #um programa para determinar qual o número de dias necessários para a epidemia infectar P ou
-#mais pessoas no total.
+#mais pessoas no total
+
+
+#Por exemplo, se no dia 0 da epidemia 1 pessoa é infectada e o fator reprodutivo R é igual a 5, então
+#  no dia 1 outras 5 pessoas são infectadas (1 + 5 = 6 pessoas no total), no dia 2 outras 25 pessoas
+#  são infectadas (1 + 5 + 25 = 31 pessoas no total), no dia 3 outras 125 pessoas são infectadas 
+# #(1 + 5 + 25 + 125 = 156 pessoas no total), e assim por diante.  
 
 n = int(input("Número de pessoas infectadas no dia 0: "))
 r = int(input("O fator reprodutivo R da infecção: "))
 p = int(input("O número alvo de pessoas infectadas: "))
 
-infectados = 0
-contador = 0
-resultado = 0
-dias = 0
-soma = 0
-i = 0
+dia = 0
+infectados_total = n  
+novos_infectados = n  
 
-while infectados < p:
-    infectados = p * r
-    soma += infectados
-    dias += 1
+while infectados_total < p:
+    novos_infectados *= r  
+    infectados_total += novos_infectados  
+    dia += 1 
 
-print(dias)
+print(dia)
