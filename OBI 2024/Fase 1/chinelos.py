db966ec19f697792ac81c90aa6e2627f8bc6b412
Henrique Silva <143834219+Henrique-nte@users.noreply.github.com>
2025-03-03 16:55:00 -0300

    OBI

--- OBI 2024/Fase 1/chinelos.py
+++ OBI 2024/Fase 1/chinelos.py
@@ -1,12 +1,22 @@
 estoque = int(input("Número de tamanhos de chinelos no estoque:"))
-
-
-for i in range(estoque):
+i = 0
+while i < estoque:
+    
     i += 1
     quantidade = int(input(f"Quantidade para o tamanho {i}:"))
+    if i != estoque:
+        continue
+    pedidos = int(input("Número de pedidos recebidos pela loja: "))
+    j = 0
+    while j < pedidos:
+        j +=1
+        efetivado = 0
 
-pedidos = int(input("Número de pedidos recebidos pela loja: "))
+        tamanho_pedido = int(input(f"O tamanho do chinelo do pedido {j}:"))
+        
+        if tamanho_pedido == quantidade:
+            efetivado += 1
+    if j == pedidos:
+        break
 
-for i in range (pedidos):
-    i += 1
-    tamanho_pedido = int(input(f"O tamanho do chinelo do pedido {i}:"))
+print(efetivado)
