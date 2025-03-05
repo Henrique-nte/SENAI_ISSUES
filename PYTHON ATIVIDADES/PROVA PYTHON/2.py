25e87b7168916f1f31e7df54cbeb1d8a194e6f57
Henrique Silva <143834219+Henrique-nte@users.noreply.github.com>
2025-03-05 15:53:14 -0300

    Update 2.py

--- PYTHON ATIVIDADES/PROVA PYTHON/2.py
+++ PYTHON ATIVIDADES/PROVA PYTHON/2.py
@@ -1,4 +1,4 @@
-#2.	A companhia ABC-LOC deseja contrair um empréstimo no Banco Mercantil de Fomento. Para tanto deseja calcular 
+#2.A companhia ABC-LOC deseja contrair um empréstimo no Banco Mercantil de Fomento. Para tanto deseja calcular 
 #quantos anos seriam necessários para o pagamento do empréstimo sabendo que o banco cobra 3,5% de juros ao mês. 
 #A companhia deseja fazer pagamentos mensais fixos no valor de 10% da dívida inicial mensalmente. Fazer um algoritmo
 #que calcule quantos meses serão necessários para o pagamento total da dívida.
