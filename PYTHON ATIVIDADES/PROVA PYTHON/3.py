c111f6c0174a3b09658cbb5b76d316cf1122e5e3
Henrique Silva <143834219+Henrique-nte@users.noreply.github.com>
2025-03-05 16:03:18 -0300

    Update 3.py

--- PYTHON ATIVIDADES/PROVA PYTHON/3.py
+++ PYTHON ATIVIDADES/PROVA PYTHON/3.py
@@ -1,16 +1,21 @@
-#3.	Faça um programa que leia um número n e mostre na tela os n  
-#e depois os n primeiros números ímpares
+#3.Faça um programa que leia um número n e mostre na tela os n primeiros números pares e depois os 
+#n primeiros números ímpares
 
 n = int(input("N = "))
 
 #primeiros números pares
-print("Números pares: ")
+print("Pares: ", end = "")
 for pares in range(1, n + 1):
+    
     if pares % 2 == 0:
-        print(f"{pares}")
-
+        
+        print(f"{pares}", end=" ")
+        
+        
+print()
 #primeiros números ímpares
-print("Números Impares: ")
+print("Impares: ", end = "")
 for impares in range(1, n + 1):
         if impares % 2 != 0:
-            print(f"{impares}")
+            print(f"{impares}", end=" ")
+            
