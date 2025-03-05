62469f49579da2da16ac37dccf5e87aad835a95e
Henrique Silva <143834219+Henrique-nte@users.noreply.github.com>
2025-03-05 16:49:27 -0300

    Update 2.py

--- PYTHON ATIVIDADES/PROVA PYTHON/2.py
+++ PYTHON ATIVIDADES/PROVA PYTHON/2.py
@@ -2,23 +2,19 @@
 #quantos anos seriam necessários para o pagamento do empréstimo sabendo que o banco cobra 3,5% de juros ao mês. 
 #A companhia deseja fazer pagamentos mensais fixos no valor de 10% da dívida inicial mensalmente. Fazer um algoritmo
 #que calcule quantos meses serão necessários para o pagamento total da dívida.
-
-emprestimo = int(input("Valor do empréstimo: "))
+emprestimo = float(input("Valor do empréstimo: "))
 
 #10% da dívida inicial
 pagamento = (emprestimo / 100) * 10
 
-total = 0
 mes = 0
-
 #O banco cobra 3,5% de juros ao mês.
-juros  = 0.035 * emprestimo
 
 #Calcule quantos meses serão necessários para o pagamento total da dívida.
-while total < emprestimo:
+while emprestimo > 0:
+        juros = emprestimo * 0.035
         emprestimo += juros
-        total += pagamento
+        emprestimo -= pagamento  
         mes += 1
-        
 
 print(f"Mês: {mes}")
