b9d82a50e48c226ee8e4b37aaa10c42fb499e41d
Henrique Silva <143834219+Henrique-nte@users.noreply.github.com>
2025-03-05 15:56:39 -0300

    Update 3.py

--- PYTHON ATIVIDADES/PROVA PYTHON/3.py
+++ PYTHON ATIVIDADES/PROVA PYTHON/3.py
@@ -2,16 +2,15 @@
 #e depois os n primeiros números ímpares
 
 n = int(input("N = "))
-j = 0
 
 #primeiros números pares
 print("Números pares: ")
-for i in range(1, n + 1):
-    if i % 2 == 0:
-        print(f"{i}")
+for pares in range(1, n + 1):
+    if pares % 2 == 0:
+        print(f"{pares}")
 
 #primeiros números ímpares
 print("Números Impares: ")
-for j in range(1, n + 1):
-        if j % 2 != 0:
-            print(f"{j}")
+for impares in range(1, n + 1):
+        if impares % 2 != 0:
+            print(f"{impares}")
