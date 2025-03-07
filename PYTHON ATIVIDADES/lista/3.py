662ece5ed95df1717652805507abb08d95db689b
Henrique Silva <143834219+Henrique-nte@users.noreply.github.com>
2025-03-07 16:21:25 -0300

    Update 3.py

--- PYTHON ATIVIDADES/lista/3.py
+++ PYTHON ATIVIDADES/lista/3.py
@@ -1,6 +1,14 @@
 #3- Em uma cidade do interior, sabe-se que de janeiro a abril de 1976 (121 dias)
 #não ocorreu temperatura inferior a 15o C e nem superior a 40o C. Fazer um 
-#programa JS que determine: 
+#programa JS que determine:
+
 #a) A menor temperatura ocorrida 
 #b) A maior temperatura ocorrida 
-#c) A temperatura média do período  
+#c) A temperatura média do período
+
+temperaturas = []
+
+for  i in range(15, 41):
+    temperaturas.append(i)
+
+print(f"{temperaturas}")
