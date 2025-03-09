29fe7dbc7572f1536afc95b760f6976b29dec68d
Henrique Santos <silvashrq@gmail.com>
2025-03-08 22:13:08 -0300

    Update chinelos.py

--- OBI 2024/Fase 1/chinelos.py
+++ OBI 2024/Fase 1/chinelos.py
@@ -1,21 +1,21 @@
 efetivado = 0
 estoque = []
 
-numero = int(input("Número de tamanhos de chinelos no estoque:"))
+numero = int(input("Número de tamanhos de chinelos no estoque: "))
 
+# Preenchendo o estoque com tamanhos e suas quantidades
 for i in range (numero):
     quantidade = int(input(f"Quantidade para o tamanho {i + 1}:"))
     estoque.append(quantidade)
 
 pedidos = int(input("Número de pedidos recebidos pela loja: "))
 
-for i in range(pedidos):
-    tamanho_pedido = int(input(f"O tamanho do chinelo do pedido {i + 1}:"))
+# Processando pedidos
+for _ in range(pedidos):
+    tamanho_pedido = int(input("Tamanho do chinelo do pedido: "))
 
-    if tamanho_pedido in estoque:
+    if tamanho_pedido in estoque and estoque[tamanho_pedido] > 0:
         efetivado += 1
-        del estoque[tamanho_pedido]
+        estoque[tamanho_pedido] -= 1  # Reduz a quantidade disponível desse tamanho
 
-      
 print(efetivado)
-
