ea2fb1705c9b315e06738f60d994e03dc73d3b9d
Henrique Silva <143834219+Henrique-nte@users.noreply.github.com>
2025-03-11 16:54:08 -0300

    Create 9.py

---
+++ PYTHON ATIVIDADES/lista/11/03/9.py
@@ -0,0 +1,8 @@
+#9 -Faça um programa que leia 10 números inteiros, armazene-os em um vetor, solicite um valor de
+#referência inteiro e:
+#a) imprima os números do vetor que são maiores que o valor referência
+#b) retorne quantos números armazenados no vetor são menores que o valor de referência
+#c) retorne quantas vezes o valor de referência aparece no vetor
+
+for i in range(10):
+    print()
