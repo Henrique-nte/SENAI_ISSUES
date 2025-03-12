db2c8f45aceec6cbc8b75b1c7090e3b6b38688ad
Henrique Silva <143834219+Henrique-nte@users.noreply.github.com>
2025-03-12 16:48:58 -0300

    Update 14.py

--- PYTHON ATIVIDADES/lista/array 3/14.py
+++ PYTHON ATIVIDADES/lista/array 3/14.py
@@ -1,3 +1,12 @@
 #14 - Você recebe um vetor de números inteiros. Escreva um programa em JS que determine a moda do vetor, ou seja,
 #  o elemento que mais frequentemente aparece. Se houver mais de um elemento com a mesma frequência máxima, 
 # retorne todos eles.
+
+vetor = [1, 2, 2, 3, 4, 5, 6, 6, 6]
+lista = []
+for i in range(len(vetor)):
+    
+    if vetor[i] == vetor[i - 1]:
+        lista.append(vetor[i])
+
+print(lista)
