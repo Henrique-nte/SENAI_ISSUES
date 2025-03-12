422a58ebbbce99773ec9bf4d8b7c6600102ef1d3
Henrique Silva <143834219+Henrique-nte@users.noreply.github.com>
2025-03-12 16:39:46 -0300

    Update 13.py

--- PYTHON ATIVIDADES/lista/array 3/13.py
+++ PYTHON ATIVIDADES/lista/array 3/13.py
@@ -2,20 +2,15 @@
 #pares os valores do
 #primeiro e nas posições impares os valores do segundo.
 
-vetor_1 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20]
 
-vetor_2 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20]
-
+vetor_1 = [45, 12, 78, 23, 56, 89, 34, 67, 90, 21, 54, 87, 32, 65, 98, 43, 76, 19, 52, 85]
+vetor_2 = [10, 29, 63, 94, 17, 38, 72, 5, 81, 47, 13, 60, 24, 59, 83, 26, 71, 4, 91, 30]
 vetor_3 = []
 
 for i in range(20):
-    if vetor_1[i] % 2 == 0:
+    if i % 2 == 1:  # Índice ímpar → Posição PAR (usa vetor_1)
         vetor_3.append(vetor_1[i])
-
-print()
-
-for i in range(20):
-    if vetor_2[i] % 2 != 0:
+    else:  # Índice par → Posição ÍMPAR (usa vetor_2)
         vetor_3.append(vetor_2[i])
 
 print(vetor_3)
