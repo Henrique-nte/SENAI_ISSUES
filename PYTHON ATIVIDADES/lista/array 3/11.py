c1c420f37c72a7ff6a21e7c5b405a9c8bdac3e07
Henrique Silva <143834219+Henrique-nte@users.noreply.github.com>
2025-03-12 14:05:51 -0300

    Update 11.py

--- PYTHON ATIVIDADES/lista/array 3/11.py
+++ PYTHON ATIVIDADES/lista/array 3/11.py
@@ -22,7 +22,7 @@ while True:
                 valor = input(f"Digite o nome do contato {i + 1}: ")
                 nomes.append(valor)
 
-                valor = input(f"Digite o telefone do contato {i + 1}: ")
+                valor = int(input(f"Digite o telefone do contato {i + 1}: "))
                 telefone.append(valor)
 
                 valor = input(f"Digite o email do contato {i + 1}: ")
@@ -30,7 +30,7 @@ while True:
         case 2:
             #EDITAR
             print("Editar contatos\n")
-            response = input("Qual o nome do contato que você deseja editar")
+            response = input("Qual o nome do contato que você deseja editar: ")
 
             for i in range(1):
                 if nomes[i] == response:
@@ -41,14 +41,53 @@ while True:
                 print("Nome não está nos seus contatos")
                 continue
 
-            print("1 - editar Nome\n 2 - editar telefone\n 3 - Editar Email")
+            print("1 - Editar Nome\n2 - Editar telefone\n3 - Editar Email\n4 - Voltar\n")
             resposta = int(input("Quais parâmetros do contato você deseja editar?"))
 
             match resposta:
                 case 1:
-                    print(posicao)
                     valor = input("Nome novo: ")
                     nomes[posicao] = valor
+
+                case 2:
+                    valor = int(input("Telefone Novo: "))
+                    telefone[posicao] = valor
+                case 3:
+                    valor = input("Email Novo: ")
+                    email[posicao] = valor
+                case _:
+                    continue
+        case 3:
+            #EXCLUIR
+            print("Excluir contatos\n")
+            response = input("Qual o nome do contato que você deseja excluir: ")
+
+            for i in range(1):
+                if nomes[i] == response:
+                    posicao = i
+                    contador = 1
+
+            if contador < 1:
+                print("Nome não está nos seus contatos")
+                continue
+
+            print("1 - Excluir Nome\n2 - Excluir telefone\n3 - Excluir Email\n4 - Voltar\n")
+            resposta = int(input("Quais parâmetros do contato você deseja editar?"))
+
+            match resposta:
+                case 1:
+                    valor = input("Nome novo: ")
+                    nomes[posicao] = valor
+
+                case 2:
+                    valor = int(input("Telefone Novo: "))
+                    telefone[posicao] = valor
+                case 3:
+                    valor = input("Email Novo: ")
+                    email[posicao] = valor
+                case _:
+                    continue
+
 
         case 4:
 
@@ -59,6 +98,6 @@ while True:
             print("-" * 60)  # Linha de separação
 
             for i in range(1):
-                print(f"{nomes[i]:<20}{telefone[i]:<20}{email[i]:<20}")
+                print(f"{nomes[i]:<20}{telefone[i]:<20}{email[i]:<20}\n")
     if resposta == 5:
         break
