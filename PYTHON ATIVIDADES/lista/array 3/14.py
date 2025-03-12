e923a558aa6af1090750a240beaf256d0689fa9b
Henrique Silva <143834219+Henrique-nte@users.noreply.github.com>
2025-03-12 16:53:14 -0300

    Update 14.py

--- PYTHON ATIVIDADES/lista/array 3/14.py
+++ PYTHON ATIVIDADES/lista/array 3/14.py
@@ -2,11 +2,16 @@
 #  o elemento que mais frequentemente aparece. Se houver mais de um elemento com a mesma frequência máxima, 
 # retorne todos eles.
 
-vetor = [1, 2, 2, 3, 4, 5, 6, 6, 6]
+vetor = [1, 2, 3, 4, 5, 6, 7, 7, 7, 7, 7, 7]
 lista = []
+contador = 0
+
 for i in range(len(vetor)):
-    
+
     if vetor[i] == vetor[i - 1]:
+        contador += 1
         lista.append(vetor[i])
+    
+    
 
 print(lista)
