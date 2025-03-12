a64253fff49b7bff22c770387feacab0eb47ded3
Henrique Silva <143834219+Henrique-nte@users.noreply.github.com>
2025-03-12 13:37:00 -0300

    Update 11.py

--- PYTHON ATIVIDADES/lista/array 3/11.py
+++ PYTHON ATIVIDADES/lista/array 3/11.py
@@ -6,28 +6,51 @@ nomes = []
 telefone = []
 email = []
 
-print("1 - Adicionar\n2 - Editar\n3 - Excluir\n4 - Exibir")
-resposta = int(input("Digite a operação que você deseja realizar: "))
+while True:
 
-print("Adicionando contatos")
+    print("1 - Adicionar\n2 - Editar\n3 - Excluir\n4 - Exibir\n5 - Sair")
+    resposta = int(input("Digite a operação que você deseja realizar: "))
 
-#ADICIONAR
-for i in range(2):
+    match resposta:
+        case 1: 
 
-    valor = input(f"Digite o nome do contato {i + 1}: ")
-    nomes.append(valor)
+            print("Adicionando contatos")
 
-    valor = input(f"Digite o telefone do contato {i + 1}: ")
-    telefone.append(valor)
+            #ADICIONAR
+            for i in range(2):
 
-    valor = input(f"Digite o email do contato {i + 1}: ")
-    email.append(valor)
+                valor = input(f"Digite o nome do contato {i + 1}: ")
+                nomes.append(valor)
 
-#EXIBIR
-print("\nContatos")
-print("=" * 60)  # Linha de separação
-print(f"{'Nome':<20}{'Telefone':<20}{'Email':<20}")  # Cabeçalho
-print("-" * 60)  # Linha de separação
+                valor = input(f"Digite o telefone do contato {i + 1}: ")
+                telefone.append(valor)
 
-for i in range(2):
-    print(f"{nomes[i]:<20}{telefone[i]:<20}{email[i]:<20}")
+                valor = input(f"Digite o email do contato {i + 1}: ")
+                email.append(valor)
+        case 2:
+            #EDITAR
+            print("Editar contatos\n")
+            response = input("Qual o nome do contato que você deseja editar")
+
+            if response in nomes:
+
+                print("1 - editar Nome\n 2 - editar telefone\n 3 - Editar Email")
+                resposta = input("Quais parâmetros do contato você deseja editar?")
+
+
+            else:
+                print("Nome não está na lista")
+                continue
+
+        case 4:
+
+            #EXIBIR
+            print("\nContatos")
+            print("=" * 60)  # Linha de separação
+            print(f"{'Nome':<20}{'Telefone':<20}{'Email':<20}")  # Cabeçalho
+            print("-" * 60)  # Linha de separação
+
+            for i in range(2):
+                print(f"{nomes[i]:<20}{telefone[i]:<20}{email[i]:<20}")
+    if resposta == 5:
+        break
