593d7d4cf4fc29bf24b69c3f4ceea0b845475b4f
Henrique Silva <143834219+Henrique-nte@users.noreply.github.com>
2025-03-14 16:51:54 -0300

    Update 12.py

--- PYTHON ATIVIDADES/lista/array 3/12.py
+++ PYTHON ATIVIDADES/lista/array 3/12.py
@@ -10,11 +10,12 @@
 indice =    [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
 sequencia = [5, 7, 6, 9, 2, 8, 4, 0, 3]
 leitura = []
+num = 0
+
 for i in range(len(indice)):
     #cada elemento possui o valor do próximo elemento do vetor a ser lido.
-    elemento = indice[i]
-    
-    leitura.append(elemento)
-    
-    
+    atual = sequencia[num]
+    leitura.append(atual)
+    num = atual - 1
+
 print(leitura)
