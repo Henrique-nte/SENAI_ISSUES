f51a88af7ec5a464e5eb868e0299acd68f0a524b
Henrique Silva <143834219+Henrique-nte@users.noreply.github.com>
2025-03-17 15:59:59 -0300

    Update 6.py

--- PYTHON ATIVIDADES/lista/11/03/6.py
+++ PYTHON ATIVIDADES/lista/11/03/6.py
@@ -1,1 +1,14 @@
-#6- Crie um programa que gere um vetor com 100 números inteiros aleatórios entre 1 e 1000. Calcule a média desses números.
+#6- Crie um programa que gere um vetor com 100 números inteiros
+# aleatórios entre 1 e 1000. Calcule a média desses números.
+
+import random
+
+numeros_aleatorios = [random.randint(1, 1000) for _ in range(10)]
+print(numeros_aleatorios)
+soma = 0
+
+for i in range(len(numeros_aleatorios)):
+    soma += numeros_aleatorios[i]
+
+media = soma / len(numeros_aleatorios)
+print(media)
