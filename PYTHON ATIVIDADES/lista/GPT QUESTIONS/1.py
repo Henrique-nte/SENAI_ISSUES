aa4e33daebd7bc2cd0d096c328e196ce79085c81
Henrique Silva <143834219+Henrique-nte@users.noreply.github.com>
2025-03-20 15:06:43 -0300

    Resolvendo...

---
+++ PYTHON ATIVIDADES/lista/GPT QUESTIONS/1.py
@@ -0,0 +1,17 @@
+#Peça ao usuário uma lista de números e depois exiba essa lista ao contrário, 
+#sem usar [::-1] ou reversed(), apenas laços de repetição.
+
+array = [1, 2, 3, 4, 5]
+lista = []
+i = len(array)
+
+
+while True:
+    i -= 1
+    lista.append(array[i])
+    if i == 0:
+        break
+
+print(f"Array original: {array}")
+print(f"Array ao contrario: {lista}")
+    
---
+++ PYTHON ATIVIDADES/lista/GPT QUESTIONS/2.py
@@ -0,0 +1,41 @@
+#Gere aleatoriamente uma lista de 4 números diferentes entre 0 e 9. Depois, 
+#peça ao usuário para adivinhar a sequência correta e informe quantos números
+#ele acertou, mas sem revelar quais.
+
+import random
+lista = []
+acertou = []
+verificador = 0
+contador = 0
+
+for i in range(4):
+    numeros_aleatorios = random.randint(0, 9)
+    lista.append(numeros_aleatorios)
+
+print("São 4 números entre 0 e 9.")
+
+print(lista) 
+
+for i in range(len(lista)):
+    adivinhar = int(input("Tente adivinhar a sequência: "))
+
+    if adivinhar in lista:
+        acertou.append(adivinhar)
+
+        
+        if adivinhar in acertou:
+            contador += 1
+
+        contador += 1
+    elif adivinhar not in lista:
+        verificador += 1
+    
+if verificador == len(lista):
+    print("Infelizmente você não acertou nenhuma")
+elif contador == len(lista):
+        print("Parabéns você acertou todos os números")
+else:
+    print(f"Você acertou {contador} números")
+
+
+        
