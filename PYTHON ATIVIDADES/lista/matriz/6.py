096f15f6a164b0456eebec7790f78b9cf91ca077
Henrique Silva <143834219+Henrique-nte@users.noreply.github.com>
2025-03-25 13:50:19 -0300

    Update 6.py

--- PYTHON ATIVIDADES/lista/matriz/6.py
+++ PYTHON ATIVIDADES/lista/matriz/6.py
@@ -1,2 +1,3 @@
 #6 – Escreva um algoritmo para armazenar valores inteiros em uma matriz (5,5). A seguir,
-#  calcular a média dos valores pares contidos na matriz e escrever seu conteúdo.
+#calcular a média dos valores pares contidos na matriz e escrever seu conteúdo.
+
