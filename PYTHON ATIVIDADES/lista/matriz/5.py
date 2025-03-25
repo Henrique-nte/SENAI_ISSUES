ad10a8d515bfb8eddbd72748ab2116e4be9fb5e2
Henrique Silva <143834219+Henrique-nte@users.noreply.github.com>
2025-03-25 13:20:33 -0300

    Update 5.py

--- PYTHON ATIVIDADES/lista/matriz/5.py
+++ PYTHON ATIVIDADES/lista/matriz/5.py
@@ -1,3 +1,10 @@
 #5- Criar um programa que leia os elementos de uma matriz inteira 4 x 4 e:
 #a) escreva os elementos da diagonal principal;
 #b) escreva todos os elementos, exceto os elementos da diagonal principal;
+
+matriz = [
+    [1, 2, 3, 4],
+    [2, 3, 4, 5],
+    [4, 3, 2, 1],
+    [9, 8, 7, 6]
+]
