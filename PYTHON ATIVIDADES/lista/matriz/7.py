06094aecfd31bda021604c4bcaeaae1ce19564da
Henrique Silva <143834219+Henrique-nte@users.noreply.github.com>
2025-03-25 15:06:23 -0300

    Update 7.py

--- PYTHON ATIVIDADES/lista/matriz/7.py
+++ PYTHON ATIVIDADES/lista/matriz/7.py
@@ -2,13 +2,13 @@
 #(supor que os valores são distintos). Após, encontrar o menor valor contido
 #na matriz e sua posição.
 
-matriz = 0
+matriz = [
+    [1, 2, 3, 4],
+    [4, 3, 2, 1],
+    [6, 7, 8, 9],
+    [14, 13, 12, 11]
+]
 valor_menor = float("inf")
 
 
-for linha in range(2):
-    for coluna in range(2):
-        valor = int(input("Digite um valor:"))
-        linha.append(valor)
-    matriz.append(linha)
 
