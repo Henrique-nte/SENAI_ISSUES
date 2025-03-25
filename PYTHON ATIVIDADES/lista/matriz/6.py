26e2296a90ca1661ef0dc39fbdedefc691385591
Henrique Silva <143834219+Henrique-nte@users.noreply.github.com>
2025-03-25 14:10:36 -0300

    Update 6.py

--- PYTHON ATIVIDADES/lista/matriz/6.py
+++ PYTHON ATIVIDADES/lista/matriz/6.py
@@ -1,3 +1,20 @@
 #6 – Escreva um algoritmo para armazenar valores inteiros em uma matriz (5,5). A seguir,
 #calcular a média dos valores pares contidos na matriz e escrever seu conteúdo.
 
+matriz = []
+
+for linha in range(2):
+    linha = []
+    for coluna in range(2):
+        valor = int(input("Digite um número: "))
+        linha.append(valor)
+    matriz.append(linha)
+
+soma_pares = 0
+
+for i in range(len(matriz)):
+    for j in range(len(matriz)):
+        if matriz[i][j] % 2 != 0:
+            soma_pares += matriz[i][j]
+            
+print(soma_pares)
