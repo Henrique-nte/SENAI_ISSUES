0d5081ec1c736a1d2f7fd47fa9261fda51882d01
Henrique Silva <143834219+Henrique-nte@users.noreply.github.com>
2025-03-31 13:17:59 -0300

    Update 9.py

--- PYTHON ATIVIDADES/lista/matriz/9.py
+++ PYTHON ATIVIDADES/lista/matriz/9.py
@@ -1,2 +1,22 @@
 #9 - Ler uma matriz 5X5 e gerar outra em que cada elemento é o cubo do elemento
 #  respectivo na matriz original.
+
+matriz = [
+    [3,2,],
+    [3,2,]  
+]
+
+lista = []
+for i in range(len(matriz)):
+    linha = []
+    for j in range(len(matriz)):
+        valor = matriz[i][j] ** 3
+        linha.append(valor)
+    lista.append(linha)
+
+for l,c in matriz:
+    print(l,"|", c)     
+print()
+for l,c in lista:
+    print(l,"|", c)
+ 
