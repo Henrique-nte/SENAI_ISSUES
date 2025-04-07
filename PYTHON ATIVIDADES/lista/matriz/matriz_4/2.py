39db6e0db03be1deeed2f22751c1e25111525a3a
Henrique Silva <143834219+Henrique-nte@users.noreply.github.com>
2025-04-07 13:52:26 -0300

    Update 2.py

---
+++ PYTHON ATIVIDADES/lista/matriz/matriz_4/2.py
@@ -0,0 +1,6 @@
+#2- Faça um programa que percorre uma lista com o seguinte
+#formato: [['Brasil', 'Itália', [10, 9]], ['Brasil', 'Espanha', [5, 7]], ['Itália',
+#'Espanha', [7,8]]]. Essa lista indica o número de faltas que cada time fez em
+#cada jogo. Na lista acima, no jogo entre Brasil e Itália, o Brasil fez 10
+#faltas e a Itália fez 9. O programa deve imprimir na tela: a) o total de faltas
+#do campeonato b) o time que fez mais faltas c) o time que fez menos faltas.
