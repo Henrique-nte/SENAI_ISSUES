2734a54dd32e2223ced664590834b75c229719d3
Henrique Silva <143834219+Henrique-nte@users.noreply.github.com>
2025-04-07 13:44:29 -0300

    Update 1.py

--- PYTHON ATIVIDADES/lista/matriz/matriz_4/1.py
+++ PYTHON ATIVIDADES/lista/matriz/matriz_4/1.py
@@ -1,34 +1,24 @@
-#1- Utilizando listas faça um programa que faça 5 perguntas para uma pessoa sobre um crime. 
-#As perguntas são: Telefonou para a vítima?; Esteve no local do crime?; Mora perto da vítima?;
-#Devia para a vítima?; Já trabalhou com a vítima? O programa deve no final emitir uma classificação
-#sobre a participação da pessoa no crime. Se a pessoa responder positivamente a 2 questões ela
-#eve ser classificada como "Suspeita", entre 3 e 4 como "Cúmplice" e 5 como "Assassino". Caso
-#contrário, ele será classificado como "Inocente".
+perguntas = ["Telefonou para a vítima?", "Esteve no local do crime?",
+"Mora perto da vítima?", "Devia para a vítima?", "Já trabalhou com a vítima?"]
 
 contador = 0
 print("1 - SIM")
 print("2 - NÃO")
- 
-primeira = input("Telefonou para a vítima?")
-if primeira == 1:
-    contador += 1
-segunda = input("Estava no local do crime?")
-if segunda == 1:
-    contador += 1
-terceira = input("Mora perto da vítima?")
-if terceira == 1:
-    contador += 1
-quarta = input("Devia para a vítima?")
-if quarta == 1:
-    contador += 1
-quinta = input("Já trabalhou com a vítima?")
 
+for i in range(len(perguntas)):
+    print(perguntas[i])
+    resposta = int(input(""))
+    if resposta == 1:
+        contador += 1
 
-if contador == 0:
-    clasificacao = "Inocente"
-if contador == 2:
-    clasificacao = "Suspeito"
-if contador >= 3 and contador <= 4:
-    clasificacao = "Inocente"
-if contador == 0:
-    clasificacao = "Inocente"
+match contador:
+    case 0:       
+        clasificacao = "inocente"
+    case 2:
+        clasificacao = "suspeito"
+    case contador if contador >= 3 and contador <= 4:
+        clasificacao = "cúmplice"
+    case 5:
+        clasificacao = "assassino"
+
+print(f"Você é: {clasificacao}")
