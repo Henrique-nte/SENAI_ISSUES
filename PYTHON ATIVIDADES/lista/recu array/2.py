f22cfcaaf760ed81cf0eaf8e1c690009f7f77974
Henrique Silva <143834219+Henrique-nte@users.noreply.github.com>
2025-04-09 16:15:38 -0300

    Update 2.py

--- PYTHON ATIVIDADES/lista/recu array/2.py
+++ PYTHON ATIVIDADES/lista/recu array/2.py
@@ -1,3 +1,9 @@
 #2-Faça um programa que armazene as horas trabalhadas de 5 funcionários em uma semana. 
 #Depois, calcule o total de horas trabalhadas e mostre o valor a ser pago a cada um, 
 #sabendo que a hora de trabalho custa R$ 20.
+
+horas = []
+
+for i in range(2):
+    hora = int(input("Horas trabalhadas: "))        
+    horas.append(hora)
