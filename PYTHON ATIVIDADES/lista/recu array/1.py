b45418e9d03783fbb3d66271e2cee4c2c644dfa9
Henrique Silva <143834219+Henrique-nte@users.noreply.github.com>
2025-04-09 13:28:59 -0300

    Resolvendo...

--- PYTHON ATIVIDADES/lista/recu array/1.py
+++ PYTHON ATIVIDADES/lista/recu array/1.py
@@ -6,13 +6,17 @@ entre = 0
 acima = 0
 
 for i in range(8):
-    peso = int(input("Pesos"))
+    peso = int(input("Peso: "))
     pesos.append(peso)
 
 for i in range(len(pesos)):
     if pesos[i] < 60:
         abaixo += 1
-    if pesos[i] >= 60 and pesos[i] <= 80:
+    elif pesos[i] >= 60 and pesos[i] <= 80:
         entre += 1
-    if pesos[i] > 80:
-        entre += 1
+    elif pesos[i] > 80:
+        acima += 1
+
+print(f"Pessoas abaixo: {abaixo}")
+print(f"Pessoas entre: {entre}")
+print(f"Pessoas acima: {acima}")
---
+++ PYTHON ATIVIDADES/lista/recu array/2.py
@@ -0,0 +1,3 @@
+#2-Faça um programa que armazene as horas trabalhadas de 5 funcionários em uma semana. 
+#Depois, calcule o total de horas trabalhadas e mostre o valor a ser pago a cada um, 
+#sabendo que a hora de trabalho custa R$ 20.
