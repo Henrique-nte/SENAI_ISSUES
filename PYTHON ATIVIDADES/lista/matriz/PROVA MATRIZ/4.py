37b8eee5111221da036e99a83af5d11b84068b7f
Henrique Silva <143834219+Henrique-nte@users.noreply.github.com>
2025-04-11 14:10:09 -0300

    Create 4.py

---
+++ PYTHON ATIVIDADES/lista/matriz/PROVA MATRIZ/4.py
@@ -0,0 +1,2 @@
+#Crie um algoritmo que gere uma matriz com dimensões definidas pelo usuário (linha x coluna),
+#preencha com números inteiros aleatórios, e calcule a soma de todos os elementos positivos.
