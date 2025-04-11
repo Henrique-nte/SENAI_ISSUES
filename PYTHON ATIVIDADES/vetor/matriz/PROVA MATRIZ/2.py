7848c6645d84b4d5d0524fa9c8c285540171cf8d
Henrique Silva <143834219+Henrique-nte@users.noreply.github.com>
2025-04-11 14:29:32 -0300

    Update 2.py

--- PYTHON ATIVIDADES/vetor/matriz/PROVA MATRIZ/2.py
+++ PYTHON ATIVIDADES/vetor/matriz/PROVA MATRIZ/2.py
@@ -9,9 +9,14 @@ matriz = [
     [1, 2, 0, 4, 5]
 ]
 
+print("Elementos da linha 3")
 for i in range(len(matriz)):
     for j in range(len(matriz)):
-        if i == 2 :
+        if i == 2:
             print(matriz[i][j])
+
+print("Elementos da coluna 3")
+for i in range(len(matriz)):
+    for j in range(len(matriz)):
         if j == 2:
             print(matriz[i][j])
