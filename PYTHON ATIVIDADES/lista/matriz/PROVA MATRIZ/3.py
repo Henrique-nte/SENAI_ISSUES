69bbe7be59460f0305bc06443f5d890120ee53aa
Henrique Silva <143834219+Henrique-nte@users.noreply.github.com>
2025-04-11 14:08:55 -0300

    Update 3.py

--- PYTHON ATIVIDADES/lista/matriz/PROVA MATRIZ/3.py
+++ PYTHON ATIVIDADES/lista/matriz/PROVA MATRIZ/3.py
@@ -5,16 +5,17 @@
 #○ Encontre o mês com maior faturamento total. 
 
 matriz = [
+    [1, 2, 5],
     [1, 2, 3],
-    [3, 1, 2],
-    [5, 2, 1],
-    [9, 4, 7],
-    [6, 2, 3],
-    [1, 2, 6] 
+    [1, 2, 3],
+    [1, 2, 3],
+    [1, 2, 3],
+    [1, 2, 3] 
 ]
 
 faturado = 0
 maior_receita = float("-inf")
+valor = float("-inf")
 
 #○ Calcule o total faturado por cliente.
 for i in range(len(matriz)):
@@ -29,14 +30,27 @@ for i in range(len(matriz)):
 print(f"Maior receita: {maior_receita}")
 #○ Encontre o mês com maior faturamento total.
 mes_faturamento_maior = 0  
+soma_1 = 0
+soma_2 = 0
+soma_3 = 0
+tamanho = len(matriz)
 
 for i in range(len(matriz)):
     for j in range(len(matriz[i])):
-        faturado += matriz[i][j]
-        print(faturado)
-        if faturado > mes_faturamento_maior:
-            mes_faturamento_maior = j
-        faturado = 0
+        if j == 0:
+            soma_1 += matriz[i][j]
+        if soma_1 > valor:
+            mes_faturamento_maior = j + 1
+            valor = soma_1
+        elif j == 1:
+            soma_2 += matriz[i][j]
+        if soma_2 > valor:
+            mes_faturamento_maior = j + 1
+            valor = soma_2
+        elif j == 2:
+            soma_3 += matriz[i][j]
+        if soma_3 > valor:
+            mes_faturamento_maior = j + 1
 
 print(f"Mês com maior faturamento: {mes_faturamento_maior}")
 
