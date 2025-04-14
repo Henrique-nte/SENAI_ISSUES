c51f8067140dd7caf6887bd8b00a047d476cca09
Henrique Silva <143834219+Henrique-nte@users.noreply.github.com>
2025-04-14 13:26:03 -0300

    Update 04.py

--- PYTHON ATIVIDADES/FUNÇÕES/questao14/04.py
+++ PYTHON ATIVIDADES/FUNÇÕES/questao14/04.py
@@ -1,11 +1,11 @@
 #Desenvolver uma estrutura modular com um procedimento que recebe através de parametro
 #um número inteiro e apresenta a tabuada deste número
 
-def mensagem(num):
+def tabuada(num):
     for i in range(10):
         i += 1
         total = num * i
         print(num, "*", i, "=", total)
 
 number = int(input("Quer saber a tabuada de qual número: "))
-mensagem(number)
+tabuada(number)
