7165ffda45b438a0c8a7dfb6c1428d1405ddcba3
Henrique Silva <143834219+Henrique-nte@users.noreply.github.com>
2025-04-14 14:02:05 -0300

    Update 2.py

--- PYTHON ATIVIDADES/vetor/recu array/2.py
+++ PYTHON ATIVIDADES/vetor/recu array/2.py
@@ -2,21 +2,8 @@
 #Depois, calcule o total de horas trabalhadas e mostre o valor a ser pago a cada um, 
 #sabendo que a hora de trabalho custa R$ 20.
 
-matriz = [
-    [5, 6, 7, 8, 9],
-    [5, 4, 7, 8, 5],
-    [5, 6, 7, 2, 9],
-    [9, 6, 7, 8, 9],
-    [1, 2, 3, 4, 5]
-]
+dias = ["Segunda", "Terça", "Quarta", "Quinta", "Sexta"]
 
-soma = 0
 
-for i in range(len(matriz)):
-    for j in range(len(matriz)):
-        soma += matriz[i][j]
-    resul = 20 * soma
-    print(f"Soma das horas trabalhadas na semana do fun {i + 1}: {soma}")
-    print(f"Valor a ser pago para o fun {i + 1}: {resul}")
-    soma = 0
-    resul = 0
+for i in range(5):
+    horas = int(input(f"Horas de {dias[i]}:")) 
