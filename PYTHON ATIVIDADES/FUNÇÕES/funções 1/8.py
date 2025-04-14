b839692d4ccbb00efd31359e461324ef59e84d19
Henrique Silva <143834219+Henrique-nte@users.noreply.github.com>
2025-04-14 16:55:35 -0300

    Update 8.py

--- PYTHON ATIVIDADES/FUNÇÕES/funções 1/8.py
+++ PYTHON ATIVIDADES/FUNÇÕES/funções 1/8.py
@@ -1,1 +1,15 @@
-#
+#Baseie-se na série de Fibonacci formada pela sequência: 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, etc.
+#Desenvolver uma estrutura
+#modular com uma função que recebe através de parâmetro um número indicando a posição do termo
+# e retorna o
+#valor correspondente na sequência de Fibonacci
+
+def calculo(posicao):
+
+    for i in range(posicao):
+        Fn = Fn - 1 + Fn - 2
+
+    return Fn
+
+posicao = 5
+print(calculo(posicao))
