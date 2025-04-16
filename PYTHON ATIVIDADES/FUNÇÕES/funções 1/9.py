682e99521aaaa4fd802b286c927734073afe5d4c
Henrique Silva <143834219+Henrique-nte@users.noreply.github.com>
2025-04-16 13:50:16 -0300

    Update 9.py

--- PYTHON ATIVIDADES/FUNÇÕES/funções 1/9.py
+++ PYTHON ATIVIDADES/FUNÇÕES/funções 1/9.py
@@ -5,3 +5,20 @@
 #Onde: PC- Peso Corporal em “kg”; Alt- altura em “cm” e I- Idade. Obs. No corpo do programa principal deve-se obter
 #os dados de entrada: sexo, peso corporal, altura e idade e a função deverá receber estes dados através de parâmetros
 #retornando o gasto energético basal calculado.
+
+def calculo(sexo, pc, alt, i):
+    #● Para os homens: GEB = 66.47 + (13.75 * PC) + (5 * Alt) – (6.76 * I);
+    
+    geb = 0
+    if sexo == "M":
+        geb = 66.47 + (13.75 * pc) + (5 * alt) - (6.76 * i)
+    elif sexo == "F":
+        geb = 655.1 + (9.56 * pc) + (1.85 * alt) - (4.67 * i)
+    return geb
+
+sexo = "f".upper()
+peso = 70
+altura = 170
+idade = 18
+
+print(calculo(sexo, peso, altura, idade))
