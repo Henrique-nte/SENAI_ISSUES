6369e2151e3d35fac14a496ceecef20e48439976
Henrique Silva <143834219+Henrique-nte@users.noreply.github.com>
2025-04-17 13:51:24 -0300

    Resolvendo...

--- PYTHON ATIVIDADES/FUNÇÕES/funções 1/gpt questions/1.py
+++ PYTHON ATIVIDADES/FUNÇÕES/funções 1/gpt questions/1.py
@@ -1,22 +1,20 @@
 #Crie uma função que receba duas palavras e retorne True se a 
 #primeira for prefixo da segunda.
 lista = []
-
-
 def calculo(word, word_2):
     contador = 0
-    
+
     for i in word_2:
         lista.append(i)
     for i in word:
         contador += 1
 
-    if contador >= 1 and contador <= 4:
+    if contador >= 2 and contador <= 4:
         return True
     else:
         return False
     
-palavra = "In"
-palavra_2 = "Inativo"
+palavra = "pinto"
+palavra_2 = "desritmia"
 
 print(calculo(palavra, palavra_2))
