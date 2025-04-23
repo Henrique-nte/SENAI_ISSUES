0499817ef93262856c5f3f4580c0e27ad0a92c64
Henrique Silva <143834219+Henrique-nte@users.noreply.github.com>
2025-04-23 16:55:11 -0300

    Update 1.py

--- PYTHON ATIVIDADES/FUNÇÕES/funções 1/gpt questions/1.py
+++ PYTHON ATIVIDADES/FUNÇÕES/funções 1/gpt questions/1.py
@@ -1,13 +1,11 @@
 def eh_prefixo(palavra1, palavra2):
     if len(palavra1) > len(palavra2):
         return False
-    for i in range(len(palavra1)):
-        if palavra1[i] != palavra2[i]:
-            return False
+      
     return True
 
 # Exemplo de uso
 palavra = "in"
 palavra_2 = "inútil"
 
-print(eh_prefixo(palavra, palavra_2))  # Saída: True
+print(eh_prefixo(palavra, palavra_2))  
