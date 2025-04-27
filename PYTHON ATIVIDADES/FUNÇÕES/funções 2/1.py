f88aeb2ede59e1216195db5d3d372d748f7c0e58
Henrique Santos <silvashrq@gmail.com>
2025-04-27 18:47:51 -0300

    1

---
+++ PYTHON ATIVIDADES/FUNÇÕES/funções 2/1.py
@@ -0,0 +1,11 @@
+#1 - Desenvolva uma estrutura modular com uma função que recebe através de parâmetro uma
+#cadeia de caracteres e retorna verdadeiro se “todos” os caracteres alfabéticos
+#da cadeia são minúsculos e falso caso contrário.
+
+def verificar(cadeia):
+  for i in cadeia:
+    if i >= "A"  and i <= "Z":
+      return False
+  return True  
+string = "abcd"
+print(verificar(string))
