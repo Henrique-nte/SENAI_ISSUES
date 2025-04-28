e1abb83452abf6d535956ec73625a66986bd3d58
Henrique Silva <143834219+Henrique-nte@users.noreply.github.com>
2025-04-28 15:37:14 -0300

    Flow

--- PYTHON ATIVIDADES/FUNÇÕES/funções 2/7.py
+++ PYTHON ATIVIDADES/FUNÇÕES/funções 2/7.py
@@ -10,22 +10,24 @@
 def palindromo(numero):
     contrario = []
     original = []
-
-    
-
+    contador = 0
     for i in numero:
         original.append(i)
 
     for i in range(len(numero) -1, -1, -1):
         contrario.append(numero[i])
 
-    for i in contrario:
-        print(i, end = '')
-        
-    print()
-    #A seguir, implementar uma estrutura modular
-    #que determine e escreva, usando a função implementada, todos os 
-    #números palíndromos entre 1000 e 10000.
+    for i in range(len(numero)):
+        if original[i] == contrario[i]:
+            contador +=1
 
-numero = 123
-print(palindromo(str(numero)))
+    if contador == len(numero):
+        return True
+    
+    return False
+
+def funcao():
+    for i in range(1000, 10000):
+        palindromo(str(i))
+    
+funcao()
