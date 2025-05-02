1a918265af907a991560daa13f1cd5a2d1c928c5
Henrique Santos <silvashrq@gmail.com>
2025-05-02 14:19:42 -0300

    Resolvendo

--- PYTHON ATIVIDADES/FUNÇÕES/funções 2/7.py
+++ PYTHON ATIVIDADES/FUNÇÕES/funções 2/7.py
@@ -1,33 +1,29 @@
-#8  - Escreva uma função que, 
+#7 - Escreva uma função que, 
 #recebendo como parâmetro um número inteiro, retorne este número
 #escrito ao contrário.
 def verificar_palindromo(numero):
     contrario = []
+    string = ''
     contador = 0
+    j = 0
     
     for i in range(len(numero) -1, -1, -1):
-        contrario.append(numero[i])
-
-    j = 0
-    for i in numero:
-        if i == contrario[j]:
-            contador += 1
-            j += 1 
-
-    string = ''
+      contrario.append(numero[i])
+      if contrario[j] == numero[j]:
+         contador += 1
+         j += 1
 
     if contador == len(numero):
-        for i in numero:
-            string += i 
-        print(string,'|',end = '')
+      for i in numero:
+          string += i 
+      print(string,'|',end = '')
         
 #A seguir, implementar uma estrutura modular
 #que determine e escreva, usando a função implementada, todos os 
 #números palíndromos entre 1000 e 10000.
 
-def de1000a10000():
+def funcao():
     for i in range(1000, 10000):
         verificar_palindromo(str(i))   
 
-print(de1000a10000())
-    
+print(funcao())
