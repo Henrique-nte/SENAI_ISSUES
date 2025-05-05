f8b5a6343a6172bc410329c120a75e55f6db80ea
Henrique Silva <143834219+Henrique-nte@users.noreply.github.com>
2025-05-05 16:22:09 -0300

    Update 14.py

--- PYTHON ATIVIDADES/FUNÇÕES/funções 2/14.py
+++ PYTHON ATIVIDADES/FUNÇÕES/funções 2/14.py
@@ -1,20 +1,3 @@
-# Questão 14 - Avaliação de aproveitamento de uma disciplina
-
-# A disciplina tem 4 provas mensais, cada uma valendo 20 pontos
-# e uma prova final que vale 40 pontos.
-
-# A nota final do aluno será a soma:
-# - das 3 maiores notas entre as 4 provas mensais
-# - com a nota da prova final
-
-# A classificação final (conceito) segue essa tabela:
-
-
-# Objetivo:
-# Criar uma sub-rotina (função) que:
-# - Recebe como entrada 4 números inteiros (as notas das provas mensais)
-# - Retorna a soma das 3 maiores entre essas 4 notas
-
 def nota_final(quatro, final):
     menor = quatro[0]
     soma_3 = 0
@@ -26,34 +9,25 @@ def nota_final(quatro, final):
 
     return soma_3 + final
 
-quatro = [7, 8, 6, 9]
-final = 8
-print(nota_final(quatro, final))
-
 final_note = nota_final
-# De 90 a 100 - Conceito A
-# De 80 a 89  - Conceito B
-# De 70 a 79  - Conceito C
-# De 60 a 69  - Conceito D
-# De 40 a 59  - Conceito E
-# De 0  a 39  - Conceito F
 
-def conceito(nota_final):
+def conceito(final_note):
+    if 90 <= final_note <= 100:
+        return 'A'
+    elif 80 <= final_note < 90:
+        return 'B'
+    elif 70 <= final_note < 80:
+        return 'C'
+    elif 60 <= final_note < 70:
+        return 'D'
+    elif 40 <= final_note < 60:
+        return 'E'
+    elif 0 <= final_note < 40:
+        return 'F'
+    else:
+        return 'Nota inválida'
 
-# Depois, criar um algoritmo principal que:
-# ● Leia os dados de 80 alunos.
-#   - Cada linha contém: número do aluno, 4 notas mensais, 1 nota da prova final
-
-# ● Para cada aluno:
-#   - Use a sub-rotina para calcular a soma das 3 maiores provas mensais
-#   - Some com a nota da prova final → isso é a nota final
-
-# ● Verifique qual é o conceito correspondente à nota final
-
-# ● Exiba para cada aluno:
-#   - Número do aluno
-#   - Nota final
-#   - Conceito
+print(conceito(final_note))
 
 alunos = [
     [1, 7.0, 8.5, 6.5, 9.0, 8.0],
@@ -65,16 +39,6 @@ alunos = [
     [7, 3.5, 4.0, 4.5, 5.0, 5.5],
     [8, 10.0, 9.5, 10.0, 9.5, 8]
 ]
-# ● Para cada aluno:
-#   - Use a sub-rotina para calcular a soma das 3 maiores provas mensais
-#   - Some com a nota da prova final → isso é a nota final
-
-# ● Verifique qual é o conceito correspondente à nota final
-
-# ● Exiba para cada aluno:
-#   - Número do aluno
-#   - Nota final
-#   - Conceito
 
 for i in range(len(alunos)):
     for j in range(1, len(alunos[0])):
