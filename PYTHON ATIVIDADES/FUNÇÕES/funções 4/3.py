84714571281886b6bc04f0722df4406b456d48b5
Henrique Silva <143834219+Henrique-nte@users.noreply.github.com>
2025-05-07 14:26:31 -0300

    Uptade

--- PYTHON ATIVIDADES/FUNÇÕES/funções 4/3.py
+++ PYTHON ATIVIDADES/FUNÇÕES/funções 4/3.py
@@ -1,2 +1,14 @@
 #Faça uma função que informe a quantidade de dígitos de um 
 # determinado número inteiro informado.
+
+def quantidade(inteiro):
+
+    digitos = 0
+    for i in inteiro:
+        digitos += 1
+
+    return digitos
+
+number = 12345
+
+print(quantidade(str(number)))
--- PYTHON ATIVIDADES/FUNÇÕES/funções 4/4.py
+++ PYTHON ATIVIDADES/FUNÇÕES/funções 4/4.py
@@ -1,2 +1,14 @@
 #Reverso do número. Faça uma função que retorne o reverso de 
 # um número inteiro informado. Por exemplo: 127 -> 721.
+
+def inverter(inteiro):
+    invertida = ''
+
+    for i in range(len(inteiro) -1, -1, -1):
+        invertida += inteiro[i]
+
+    return invertida
+
+sentence = 127
+
+print(inverter(str(sentence)))
